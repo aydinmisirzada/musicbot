f0e966ffa23fa3773d762a7153263e41246d55a4
Aydin Misirzade <aydinmisirzade@hotmail.com>
2020-12-10 17:00:58 +0100

    commit

--- bot.py
+++ bot.py
@@ -18,19 +18,19 @@ dispatcher = updater.dispatcher
 
 def music(update: Update, context: CallbackContext):
 
-    # if len(context.args) == 0:
-    #     msg = 'Send a message in format "/music link_to_youtube"'
-    #     context.bot.send_message(
-    #                 chat_id=update.effective_chat.id,
-    #                 text=
-    #                 msg,
-    #                 reply_to_message_id=update.effective_message.message_id,
-    #                 parse_mode=ParseMode.HTML,
-    #     )
-    #     return
+    if len(context.args) == 0:
+        msg = 'Send a message in format "/music link_to_youtube"'
+        context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text=
+                    msg,
+                    reply_to_message_id=update.effective_message.message_id,
+                    parse_mode=ParseMode.HTML,
+        )
+        return
 
     music_src = context.args[0]
-    yt = YouTube(music_src).streams.filter(only_audio=True).first().download(output_path="../tmp")
+    # yt = YouTube(music_src).streams.filter(only_audio=True).first().download(output_path="../tmp")
     print("Hello",yt)
     context.bot.send_message(
                 chat_id=update.effective_chat.id,
@@ -38,7 +38,7 @@ def music(update: Update, context: CallbackContext):
                 "Hello",
                 parse_mode=ParseMode.HTML,
     )
-    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(yt, 'rb'))
+    # context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(yt, 'rb'))
 
 
 dispatcher.add_handler(CommandHandler("music", music))
