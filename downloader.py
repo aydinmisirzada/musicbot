d4a6e3cad9824e586f182bbf669eb1e7ac0faa53
Aydin Misirzade <aydinmisirzade@hotmail.com>
2020-12-31 14:13:56 +0100

    commit

--- downloader.py
+++ downloader.py
@@ -7,6 +7,7 @@ class Downloader:
     author = "Unknown Author"
 
     def __init__(self,src):
+        self.check_if_youtube(src)
         yt = YouTube(src)
         if yt is None:
             raise Exception("The audio can't be found")
@@ -18,8 +19,14 @@ class Downloader:
         self.extract_author_n_title()
 
     def extract_author_n_title(self):
-        pattern = "(.*)[-–—](.*)"
+        pattern = "(.*)-(.*)"
         matchObject = re.match(pattern,self.title)
         if matchObject is not None:
             self.author =  matchObject.group(1).rstrip() or self.author
             self.title = matchObject.group(2).strip() or self.title
+
+    def check_if_youtube(self,str):
+        pattern = "https?://youtu(be\.com|\.be)/\w+"
+        matchObject = re.match(pattern,str)
+        if matchObject is None:
+            raise Exception("Invalid YouTube link")
